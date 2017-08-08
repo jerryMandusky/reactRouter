eacba64f713584629416f07ca02602ae3f8fa88c
Jeremiah Hunsicker <jeremiah.hunsicker@optum.com>
2017-08-08 16:43:06 -0400

    on lecture 146

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom';
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import promise from 'redux-promise';
 //import App from './components/app'; - used a mainscource for bootstappin applicaion in previous projects
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
-import promise from 'redux-promise';
+import PostsNew from './components/posts_new';
+import PostsShow from './components/posts_show';
+
 
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
@@ -16,8 +18,12 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
     <div>
-      <Route path="/" component={PostsIndex} />
-      </div>
+      <Switch>
+        <Route path="/posts/new" component={PostsNew} />//most specific route first
+        <Route path="/posts/:id" component={PostsShow} />
+        <Route path="/" component={PostsIndex} />
+      </Switch>
+    </div>
     </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
